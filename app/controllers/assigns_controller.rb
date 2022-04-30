d8975cbc6675b3420494360af45f850ee7b2a438
satokofuwa <sato.fuwa@nifty.com>
2022-04-30 12:34:25 +0900

    管理者とログインユーザーが自分のアサインを削除出来る機能を追加

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -15,12 +15,9 @@ class AssignsController < ApplicationController
   end
 
   def destroy
-    if current_user.keep_team_id || current_user.id?
-
     assign = Assign.find(params[:id])
     destroy_message = assign_destroy(assign, assign.user)
     redirect_to team_url(params[:team_id]), notice: destroy_message
-    end
   end
 
   private
@@ -29,17 +26,22 @@ class AssignsController < ApplicationController
   end
 
   def assign_destroy(assign, assigned_user)
-    if assigned_user == assign.team.owner
-      I18n.t('views.messages.cannot_delete_the_leader')
-    elsif Assign.where(user_id: assigned_user.id).count == 1
-      I18n.t('views.messages.cannot_delete_only_a_member')
-    elsif assign.destroy
-      set_next_team(assign, assigned_user)
-      I18n.t('views.messages.delete_member')
+    if  assigned_user == assign.team.owner  #チームオーナーとアサイン(チームに入っている)ユーザが一緒
+      I18n.t('views.messages.cannot_delete_the_leader') #リーダーは削除できません
+    elsif Assign.where(user_id: assigned_user.id).count == 1 #アサインユーザーの数が一つ
+      I18n.t('views.messages.cannot_delete_only_a_member') #このチームにしか所属していないので削除できません
+    elsif current_user.id == assign.team.owner.id || current_user.id == assigned_user.id #チームオーナー
+        assign.destroy
+        set_next_team(assign,assigned_user)
+        I18n.t('views.messages.delete_member') #メンバーを削除しました
+    elsif current_user.id != assigned_user.id
+      I18n.t('views.messages.only_leader_can_delete_a_member') #リーダーだけが削除できます。
     else
-      I18n.t('views.messages.cannot_delete_member_4_some_reason')
+      I18n.t('views.messaes.cannot_delete_member_4_some_reason')
     end
   end
+
+
 
   def email_exist?
     team = find_team(params[:team_id])
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,7 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_team, only: %i[show edit update destroy]
+  before_action :check_owner, only: %i[edit update]
 
 
   def index
@@ -59,4 +60,11 @@ class TeamsController < ApplicationController
   def team_params
     params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id]
   end
+
+  def check_owner
+     set_team
+     @team.owner == current_user
+    redirect_to teams_url, notice: '失敗しました' if @team.owner != current_user 
+  end
+
 end
